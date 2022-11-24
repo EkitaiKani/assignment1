fc131191c8115323632a6a4f4053e3df6ed6e8e1
EkitaiKani <kagerokaiser@gmail.com>
2022-11-24 22:52:52 +0800

    I FIXED THE NAV BAR (for mobile)

---
+++ script.js
@@ -0,0 +1,9 @@
+/* this is for the nav on mobile */
+
+function openNav() {
+    document.getElementById("Nav").style.height = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("Nav").style.height = "0%";
+  }
